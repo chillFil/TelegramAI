91679f2d166952276658107ae00710d1aa69eb0c
Filippo Carletti <filippo.carletti05@gmail.com>
2023-09-21 09:50:45 +0200

    Working Google search
    Integration with Google APIs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+// Libraries and APIs
+// const { Configuration, OpenAIApi } = require("openai");
 const { Telegraf } = require("telegraf")
 const { join } = require("path")
 const { stringify } = require("querystring")
 const { google } = require("googleapis")
 
 
+// Environment variables
 require("dotenv").config({ 
     path: join(__dirname, "../.env") 
 })
@@ -11,9 +14,10 @@ require("dotenv").config({
 const {
     BOT_TOKEN,
     GG_API_KEY,
-    IMG_SEARCH_ID
+    IMG_SEARCH_ID,
+    // OPENAI_API_KEY,
+    // OPEN_AI_ID
 } = process.env
-
 if (!BOT_TOKEN) {
     throw new Error("BOT_TOKEN must be provided!")
 }
@@ -23,42 +27,59 @@ if (!GG_API_KEY) {
 if (!IMG_SEARCH_ID) {
     throw new Error("IMG_SEARCH_ID must be provided!")
 }
+// if (!OPENAI_API_KEY) {
+//     throw new Error("OPENAI_API_KEY must be provided!")
+// }
+// if (!OPEN_AI_ID) {
+//     throw new Error("OPEN_AI_ID must be provided!")
+// }
+
+// Initialize bot
+const bot = new Telegraf(BOT_TOKEN);
+//Custom Search
 const customsearch = google.customsearch('v1');
+// Setting OpenAiAPI
+// const configuration = new Configuration({
+//     organization: OPEN_AI_ID,
+//     apiKey: OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+// const response = openai.listEngines();
 
-customsearch.cse.list({
-    auth: GG_API_KEY,
-    cx: IMG_SEARCH_ID,
-    q: 'cat',
-}).then((result) => {
-    console.log(result.data)
-})
-
-const bot = new Telegraf(BOT_TOKEN)
-
-// Triggered when start the bot (or send /start command)
-bot.start((ctx) => {
+// Bot commands
+bot.start((ctx) => { //start
     ctx.reply("Hello!")
 })
 
-bot.command("promote", (ctx) => {
+bot.command("promote", (ctx) => { //promote
     ctx.reply("IG: @_chillfil_, @fillo.dalla")
 })
 
-bot.command("echo", (ctx) => {
-    ctx.reply(ctx.message.text)
-})
-
-//Random pic generator
-bot.command("pic", (ctx) => {
-    let time = new Date().getTime()
-    const pic = "https://picsum.photos/?time"
-    ctx.replyWithPhoto(pic)
-})
-
-
-bot.command("custom", (ctx) => {
-    str = ctx.message.text.replace("/custom ", "")
+bot.command("echo", (ctx) => { //echo
+    str = ctx.message.text.replace("/echo ", "")
     ctx.reply(str)
+})
+
+bot.command("pic", (ctx) => { //random pic
+    let str = ctx.message.text.replace("/pic ", "")
+    customsearch.cse.list({
+        auth: GG_API_KEY,
+        cx: IMG_SEARCH_ID,
+        q: str,
+        searchType: 'image',
+        num: 10,
+        safeSearch: 'off',
+        disableWebSearch: true
+    }).then((result) => {
+        ctx.replyWithPhoto(result.data.items[Math.floor(Math.random() * 10)].link)
+    }).catch((err) => {
+        ctx.reply(err)
+    })
+})
+
+bot.command("help", (ctx) => {
+    let reply = "/start \n/promote \n/echo <text you want to cut out> \n/pic <img you want to search>"
+    ctx.reply(reply)
 })
 
 bot.on("message", (ctx) => {
@@ -70,16 +91,15 @@ bot.on("message", (ctx) => {
     if (msg.includes("how are you")) {
         ctx.reply("Fine, thanks!")
     }
-    if (msg.includes("cat")) {
-        ctx.replyWithPhoto("https://im.indiatimes.in/content/2023/Sep/Blue-Smurf-Cat-meme_6506cc84d1090.png")
-    }
 })
 
+
+// Start bot
 bot.launch({
     dropPendingUpdates: true
 })
-
 console.log("Bot started!")
+
 
 // Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"))
